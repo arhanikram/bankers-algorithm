52277605daf0c48acfd4dec2ad7ef3c83836ec21
arhanikram <33167443+arhanikram@users.noreply.github.com>
2020-08-02 23:51:06 -0400

    Create 170399300_a04.c

---
+++ A04/170399300_a04.c
@@ -0,0 +1,47 @@
+/*
+ -------------------------------------
+ File:    170399300_a04.c.txt
+ Assignment 4 Functions Source Code File
+ -------------------------------------
+ Author:  Arhan Ikram
+ ID:      170399300
+ Email:   ikra9300@mylaurier.ca
+ GitHub:  arhanikram
+ Repository: https://github.com/arhanikram/bankers-algorithm
+ Version: 2020-08-02
+ -------------------------------------
+ To Run:
+rename makefile.mk to makefile, run:
+mv makefile.mk Makefile
+
+the Makefile will be in the same folder as your .c file to compile run the following command
+make
+(this will compile your program with warnings turned into errors)
+
+If you instead run
+make warnings_OK
+(this will compile your program with warnings enabled but not fatal)
+ -------------------------------------
+ */
+ 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+ 
+ 
+ int main(int argc, char* argv[]){
+	if(argc<2){
+		printf("Input file name missing...exiting with error code -1\n");
+		return -1;
+	}
+
+
+ 
+ return 0;
+ }
+ 
+ int readFile(char* fileName){
+	return 0;
+ }
